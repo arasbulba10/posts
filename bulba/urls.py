c5eed894ab2e5abb1e168cacd97d6d08c818d55f
arasbulba10 <araz@webzool.com>
2020-09-13 20:09:16 +0400

    New post feature

--- bulba/urls.py
+++ bulba/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
-    
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
